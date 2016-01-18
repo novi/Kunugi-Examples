476c948329076ef10276a31c8840270421745354
Yusuke Ito <novi.mad@gmail.com>
2016-01-19 01:23:58 +0900

    fix tab spaces, sorry

--- BasicApp/Context.swift
+++ BasicApp/Context.swift
@@ -3,12 +3,12 @@ import Kunugi
 import Inquiline
 
 class Context: ContextBox {
-	var context: [ContextType] = []
-	var request: Request
+    var context: [ContextType] = []
+    var request: Request
     
-	var method: Method
-	var path: String
-	var parameters: [String: String] = [:]
+    var method: Method
+    var path: String
+    var parameters: [String: String] = [:]
 	
     init(request: RequestType, method: Method) {
         self.request = Request(method: request.method, path: request.path, headers: request.headers, body: request.body)
